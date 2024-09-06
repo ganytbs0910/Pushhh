4a58dc9b200b4d481d784cde797b81df2a1be972
ganytbs0910 <nakakou2002@icloud.com>
2024-09-06 15:49:00 +0900

    ある程度動くチュートリアルを実装

--- Assets/Scripts/SlotTutorial.cs
+++ Assets/Scripts/SlotTutorial.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Cysharp.Threading.Tasks;
 using UniRx;
 using System;
 using System.Threading;
+using DG.Tweening;
 
 public class SlotTutorial : MonoBehaviour
 {
+    [SerializeField] private GameObject slotTutorialPanel;
+    [SerializeField] private Button tutorialPanelCloseButton, closeButton;
+    [SerializeField] private Image unmaskedPanel, tutorialPanel;
+    [SerializeField] private Image arrowImage;
     [SerializeField] private UIController uiController;
     private const string FirstLaunchKey = "IsFirstLaunch";
     private const string VersionKey = "GameVersion";
@@ -17,33 +23,45 @@ public class SlotTutorial : MonoBehaviour
     {
         CheckFirstLaunch().Forget();
     }
+    void Start()
+    {
+        tutorialPanelCloseButton.onClick.AddListener(() =>
+        {
+            tutorialPanel.gameObject.SetActive(false);
+            unmaskedPanel.gameObject.SetActive(true);
+            //arrowImageを左下に-50移動させるアニメーションを繰り返す
+            arrowImage.rectTransform.DOAnchorPos(new Vector2(-3, -3), 0.5f).SetLoops(-1, LoopType.Yoyo);
+        });
+        closeButton.onClick.AddListener(() =>
+        {
+            tutorialPanel.gameObject.SetActive(false);
+            unmaskedPanel.gameObject.SetActive(true);
+            //arrowImageを左下に-3移動させるアニメーションを繰り返す
+            arrowImage.rectTransform.DOAnchorPos(new Vector2(-3, -3), 0.5f).SetLoops(-1, LoopType.Yoyo);
+        });
+    }
 
     private async UniTaskVoid CheckFirstLaunch()
     {
         string savedVersion = PlayerPrefs.GetString(VersionKey, "");
         isFirstLaunch.Value = string.IsNullOrEmpty(savedVersion) || savedVersion != CurrentVersion;
-
         if (isFirstLaunch.Value)
         {
             await FirstLaunchProcess();
             PlayerPrefs.SetString(VersionKey, CurrentVersion);
             PlayerPrefs.Save();
         }
-        else
-        {
-            Destroy(gameObject);
-        }
+        else Destroy(gameObject);
     }
 
     private async UniTask FirstLaunchProcess()
     {
         Debug.Log("初回起動時の処理を実行中...");
-
         // UIControllerの初期化を待つ
         await UniTask.WaitForEndOfFrame();
+        slotTutorialPanel.gameObject.SetActive(true);
         // その他の初回起動時の処理
-
-
+        tutorialPanel.gameObject.SetActive(true);
         Debug.Log("初回起動時の処理が完了しました。");
     }
 
